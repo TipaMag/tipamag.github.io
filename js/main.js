f5c765394bd072dff11fe03ca1feed613ca6c708
Svyat <perynsky@gmail.com>
2019-10-06 19:33:16 +0300

    create to-do list

---
+++ js/main.js
@@ -0,0 +1,49 @@
+let addMessage = document.querySelector('.message'),
+    addButton = document.querySelector('.add'),
+    todo = document.querySelector('.list')
+
+let todoList = [];
+
+if(localStorage.getItem('todo')) {
+    todoList = JSON.parse(localStorage.getItem('todo'));
+    displayMessage();
+};
+
+addButton.addEventListener('click', function(){
+    let newTodo = {
+        todo: addMessage.value,
+        checked: false,
+        important: false
+    };
+    todoList.push(newTodo);
+    displayMessage();
+    localStorage.setItem('todo', JSON.stringify(todoList));
+});
+
+function displayMessage() {
+    let displayMessage = '';
+    todoList.forEach(function(item, i){
+        displayMessage += `
+        <li class='list-item'>
+            <input id='item_${i}'  type='checkbox' ${item.checked ? 'checked' : ''}>
+            <label for='item_${i}' class='${item.important ? 'important' : ''}'>${item.todo}</label>
+        </li>
+        `;
+        todo.innerHTML = displayMessage;
+    })
+};
+
+todo.addEventListener('change', function(event){
+    let idInput = event.target.getAttribute('id');
+    let forLable = todo.querySelector(`[for=${idInput}]`);
+    let valueLable = forLable.innerHTML;
+
+    todoList.forEach(function(item){
+        if(item.todo === valueLable) {
+            item.checked = !item.checked;
+            item.important = !item.important;
+        }
+        localStorage.setItem('todo', JSON.stringify(todoList));
+    })
+    displayMessage();
+});
